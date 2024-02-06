46ace3df16de31e7f40708e211b45029cb1f2e64
saurinya <rishimagiawala@gmail.com>
2024-02-06 18:28:45 -0500

    Added stuff

---
+++ components/examplechart.js
@@ -0,0 +1,34 @@
+// charts.tsx/jsx
+
+'use client' // if you use app dir, don't forget this line
+
+import dynamic from "next/dynamic";
+const ApexChart = dynamic(() => import("react-apexcharts"), { ssr: false });
+
+
+export default function ExampleChart(props){
+
+    const option = {
+      chart: {
+        id: "basic-bar"
+      },
+      xaxis: {
+        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+      }
+      }
+
+    const series = [{
+      name: "series-1",
+      data: [30, 40, 45, 50, 49, 60, 70, 91]
+    }]
+
+    return(
+
+
+        <div className="bg-white w-96 h-96 rounded-lg drop-shadow-lg m-1">
+
+            <ApexChart type={props.type} options={option} series={series} height={'100%'} width={'100%'} />
+        </div>
+    )
+    
+}
