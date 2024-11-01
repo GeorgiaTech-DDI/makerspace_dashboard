802c024344d9859480b9b196a9b1c8e3aa773e31
Cole Gensheimer <92688065+cgensheimer@users.noreply.github.com>
2024-11-01 09:43:20 -0400

    build overrides

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/** @type {import('next') -NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // 11 WARN 1!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // 11 WARN 1!
+    ignoreBuildErrors: true,
+  },
+};
 
 export default nextConfig;
