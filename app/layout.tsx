7c97555747defec70d0d4fd9bd8479ec952d75e7
Cole Gensheimer <92688065+cgensheimer@users.noreply.github.com>
2024-11-09 00:00:25 -0500

    Theme Provider and Styling Changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { StorageVersionCheck } from "./flush";
+import { ThemeProvider } from "@/components/theme-provider"
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -30,7 +31,14 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <StorageVersionCheck />
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
